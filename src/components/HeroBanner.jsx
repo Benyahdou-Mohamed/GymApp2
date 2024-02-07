0c803e5746673e836f3507304761844a18b7e471
unknown <benyahdou.moh29@hotmail.com>
2024-02-07 14:35:39 +0100

    first commit

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { Box,Stack,Typography,Button} from '@mui/material';
 export default function HeroBanner(){
   return (
-    <div>HeroBanner</div>
+    <Box sx={{mt:{lg:'212px',xs:'70px'},ml:{sm:"50px"}}}>
+      <Typography color='#FF2625'
+      fontWeight="600"
+      fontSize="26px"
+      >
+         FitFlex Hub
+      </Typography>
+      <Typography fontWeight="700"
+      sx={{fontSize:{lg:'44px',xs:'40px'}}}
+      >
+         Flex Your Fitness Potential!
+      </Typography>
+      <Typography fontSize="22px" lineHeight={"35px"} mb={3}>
+        Check out the most effective Excersises
+      </Typography>
+      <Button variant="contained" color="error">Explore Excercices</Button>
+    </Box>
   )
 }
 
